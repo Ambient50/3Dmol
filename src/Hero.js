a7e5a401a92b09715248682cd119f802e61e153e
sam <116031274+Ambient50@users.noreply.github.com>
2022-11-06 11:53:31 +0530

    YESS
    YESHHH

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Hero = () => {
   return (
    
-    <div className='viewer_3Dmoljs threed-render' data-pdb='2POR' data-backgroundcolor='0xffffff' data-style='stick' data-ui='true'></div> 
+    <div className='viewer_3Dmoljs threed-render' data-pdb='7R04' data-backgroundcolor='0xffffff' data-style='stick' data-ui='true'></div> 
     
   )
 }
