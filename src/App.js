c71abb5ed1565889c90acb82edf9e9f95db7e7c0
sam <116031274+Ambient50@users.noreply.github.com>
2022-11-06 11:33:58 +0530

    niya helped me
    niya god

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./header";
+import Footer from "./footer";
+import Hero from "./Hero";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+    
+const Hero = () => {
+  return (
+   
+    <div className='viewer_3Dmoljs threed-render' data-pdb='2POR' data-backgroundcolor='0xffffff' data-style='stick' data-ui='true'></div> 
+    
+  )
+}
+
+export default Hero
